2d57939294a9edc0aac9e040f36bd6c6abf99a35
Arjun Kumar <aksir648@gmail.com>
2025-01-04 09:23:17 +0530

    Update UserDAO.java

--- src/main/java/ecom/example/ecommerce/DAO/UserDAO.java
+++ src/main/java/ecom/example/ecommerce/DAO/UserDAO.java
@@ -1,1 +1,19 @@
 
+package com.example.dao;
+
+import com.example.models.User;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO {
+    private static List<User> users = new ArrayList<>();
+
+    public boolean saveUser(User user) {
+        return users.add(user);
+    }
+
+    public List<User> getAllUsers() {
+        return users;
+    }
+}
+
