caf8994fcea186d8058a5445f8576d8278e3d3a2
Arjun Kumar <aksir648@gmail.com>
2025-01-04 08:37:54 +0530

    Update productDAO.java

--- ecommerce/ecomproject/dao/productDAO.java
+++ ecommerce/ecomproject/dao/productDAO.java
@@ -1,12 +1,30 @@
-package ecommmerce.ecomproject.dao;
+package com.example.ecommerce.controller;
 
-import com.yourprojectname.model.Product;
-import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import com.example.ecommerce.service.ProductService;
 
-public interface ProductDAO {
-    void addProduct(Product product);
-    Product getProduct(int id);
-    List<Product> getAllProducts();
-    void updateProduct(Product product);
-    void deleteProduct(int id);
+@WebServlet("/products")
+public class ProductServlet extends HttpServlet {
+    private ProductService productService = new ProductService();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("products", productService.getAllProducts());
+        request.getRequestDispatcher("/WEB-INF/views/products.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        double price = Double.parseDouble(request.getParameter("price"));
+        productService.addProduct(name, price);
+        response.sendRedirect("products");
+    }
 }
+
+
