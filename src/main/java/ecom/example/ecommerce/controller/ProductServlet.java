a1c1ed0d2a365a1ba8a346a92e305224593af152
Arjun Kumar <aksir648@gmail.com>
2025-01-04 11:50:48 +0530

    Update ProductServlet.java

--- src/main/java/ecom/example/ecommerce/controller/ProductServlet.java
+++ src/main/java/ecom/example/ecommerce/controller/ProductServlet.java
@@ -1,1 +1,29 @@
 
+package com.example.ecommerce.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import com.example.ecommerce.service.ProductService;
+
+@WebServlet("/products")
+public class ProductServlet extends HttpServlet {
+    private ProductService productService = new ProductService();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("products", productService.getAllProducts());
+        request.getRequestDispatcher("/WEB-INF/views/products.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        double price = Double.parseDouble(request.getParameter("price"));
+        productService.addProduct(name, price);
+        response.sendRedirect("products");
+    }
+}
