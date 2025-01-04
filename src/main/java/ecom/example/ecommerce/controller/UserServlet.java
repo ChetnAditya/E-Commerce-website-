8054de90a7b2fcf4a8da99a924f950a4025120bd
Arjun Kumar <aksir648@gmail.com>
2025-01-04 11:51:46 +0530

    Update UserServlet.java

--- src/main/java/ecom/example/ecommerce/controller/UserServlet.java
+++ src/main/java/ecom/example/ecommerce/controller/UserServlet.java
@@ -1,1 +1,23 @@
+package com.example.ecommerce.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import com.example.ecommerce.service.UserService;
+
+@WebServlet("/register")
+public class UserServlet extends HttpServlet {
+    private UserService userService = new UserService();
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        userService.registerUser(username, password);
+        response.sendRedirect("profile");
+    }
+}
 
