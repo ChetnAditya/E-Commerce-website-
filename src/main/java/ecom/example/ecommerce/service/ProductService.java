ee6859f80b3a18c0be8487e6368d445c832e2d90
Arjun Kumar <aksir648@gmail.com>
2025-01-04 09:45:43 +0530

    Create ProductService.java

---
+++ src/main/java/ecom/example/ecommerce/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.example.ecommerce.service;
+
+import com.example.ecommerce.model.Product;
+import com.example.ecommerce.dao.ProductDAO;
+import java.util.List;
+
+public class ProductService {
+    private ProductDAO productDAO = new ProductDAO();
+
+    public List<Product> getAllProducts() {
+        return productDAO.getAllProducts();
+    }
+
+    public void addProduct(String name, double price) {
+        Product product = new Product(name, price);
+        productDAO.addProduct(product);
+    }
+}
